c208a0c96146eef3091ec2b0f720eb51ef83b57b
UnofficialTranslator <cooldaffie9@gmail.com>
2025-12-09 12:10:41 +0100

    Patterns

--- Assets/MinigameDesign/Scripts/DesignPattern.cs
+++ Assets/MinigameDesign/Scripts/DesignPattern.cs
@@ -4,7 +4,13 @@ public class DesignPattern : MonoBehaviour
 {
     [SerializeField] GameObject[] patterns;
 
-    [SerializeField] GameObject[] pattern_stripes;
+    [SerializeField] GameObject[] v_pattern_stripes;
+    [SerializeField] GameObject[] h_pattern_stripes;
+    [SerializeField] GameObject[] d_pattern_stripes;
+    [SerializeField] GameObject[] pattern_dots;
+    [SerializeField] GameObject[] pattern_squares;
+    [SerializeField] GameObject[] pattern_star;
+    [SerializeField] GameObject[] pattern_logo;
 
     private void Start()
     {
@@ -20,13 +26,92 @@ public class DesignPattern : MonoBehaviour
 
     }
 
-    public void Pattern_Stripes(bool toggle_value)
+    public void V_Pattern_Stripes(bool toggle_value)
     {
         if (toggle_value)
         {
             Hide();
 
-            foreach (GameObject item in pattern_stripes)
+            foreach (GameObject item in v_pattern_stripes)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+
+    public void H_Pattern_Stripes(bool toggle_value)
+    {
+        if (toggle_value)
+        {
+            Hide();
+
+            foreach (GameObject item in h_pattern_stripes)
+            {
+                item.SetActive(true);
+            }
+        }
+     }
+
+    public void D_Pattern_Stripes(bool toggle_value)
+    {
+        if (toggle_value)
+        {
+            Hide();
+
+            foreach (GameObject item in d_pattern_stripes)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    public void Pattern_Dots(bool toggle_value)
+    {
+        if (toggle_value)
+        {
+            Hide();
+
+            foreach (GameObject item in pattern_dots)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    public void Pattern_Squares(bool toggle_value)
+    {
+        if (toggle_value)
+        {
+            Hide();
+
+            foreach (GameObject item in pattern_squares)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    public void Pattern_Star(bool toggle_value)
+    {
+        if (toggle_value)
+        {
+            Hide();
+
+            foreach (GameObject item in pattern_star)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    public void Pattern_Logo(bool toggle_value)
+    {
+        if (toggle_value)
+        {
+            Hide();
+
+            foreach (GameObject item in pattern_logo)
             {
                 item.SetActive(true);
             }
