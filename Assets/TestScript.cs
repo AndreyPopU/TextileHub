5513bc801b71670935a45d35634d4280d0459237
AndreyPopov <andreypopu@gmail.com>
2026-01-06 11:54:43 +0100

    Random Changes

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    void Start()
+    public int score;
+    public GameObject[] consequences;
+
+    //void Start()
+    //{
+    //    Color givenColor = new Color(.3f, .5f, 1, 1);
+    //    string primaryHex = givenColor.ToHexString();
+    //    print(primaryHex);
+    //}
+
+    private void Update()
     {
-        Color givenColor = new Color(.3f, .5f, 1, 1);
-        string primaryHex = givenColor.ToHexString();
-        print(primaryHex);
+        if (Input.GetKeyDown(KeyCode.G)) DecideConsequence();
     }
+
+    private void DecideConsequence() => consequences[score].gameObject.SetActive(true);
 }
